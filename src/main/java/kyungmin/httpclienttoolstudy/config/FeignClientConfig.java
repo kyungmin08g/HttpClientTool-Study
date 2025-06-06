56749b3801e7b186c4738ec016c93a98b880664a
Kim KyungMin <k.kyungmin2892@gmail.com>
2025-06-07 01:40:39 +0900

    [UPDATE] API 로그 범위 설정

--- src/main/java/kyungmin/httpclienttoolstudy/config/FeignClientConfig.java
+++ src/main/java/kyungmin/httpclienttoolstudy/config/FeignClientConfig.java
@@ -1,8 +1,16 @@
 package kyungmin.httpclienttoolstudy.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.logging.Level;
 
 @Configuration
 public class FeignClientConfig {
+
+  @Bean
+  public Level getLogLevel() {
+    return Level.ALL;
+  }
 
 }
