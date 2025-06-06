7ccaa08fd817f0077efb1bf5a6e2581749e305b3
Kim KyungMin <k.kyungmin2892@gmail.com>
2025-06-07 00:39:57 +0900

    [CHORE] Swagger 문서를 위한 설명 필요

--- src/main/java/kyungmin/httpclienttoolstudy/controller/APIController.java
+++ src/main/java/kyungmin/httpclienttoolstudy/controller/APIController.java
@@ -1,5 +1,7 @@
 package kyungmin.httpclienttoolstudy.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/api")
+@Tag(name = "나이스 관련 API")
 public class APIController {
 
   @GetMapping(value = "/request")
+  @Operation(summary = "학교 조회 API 호출")
   public ResponseEntity<Void> apiRequest() {
     return ResponseEntity.ok().build();
   }
