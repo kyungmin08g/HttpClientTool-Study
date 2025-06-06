0d76a94b1ab6372eb00b6b1c1de887efce7549ef
Kim KyungMin <k.kyungmin2892@gmail.com>
2025-06-06 23:46:15 +0900

    [CHORE] Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# HttpClientTool-Study
---
+++ src/main/java/kyungmin/httpclienttoolstudy/HttpClientToolStudyApplication.java
@@ -0,0 +1,11 @@
+package kyungmin.httpclienttoolstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpClientToolStudyApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(HttpClientToolStudyApplication.class, args);
+  }
+}
