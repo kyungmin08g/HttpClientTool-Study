c74b3e9a9e29c0e31cb87addbf92cc2043e4e4d7
Kim KyungMin <k.kyungmin2892@gmail.com>
2025-06-07 01:34:25 +0900

    [FEAT] 서비스단 구현

--- src/main/java/kyungmin/httpclienttoolstudy/service/APIService.java
+++ src/main/java/kyungmin/httpclienttoolstudy/service/APIService.java
@@ -1,13 +1,19 @@
 package kyungmin.httpclienttoolstudy.service;
 
+import kyungmin.httpclienttoolstudy.controller.response.GetSchoolInfoResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class APIService {
+  private final NeisClient neisClient;
 
-  public void getApiRequest() {
+  @Value(value = "${open-api.key}")
+  private String appKey;
 
+  public GetSchoolInfoResponse getApiRequest(String schoolName) {
+    return neisClient.getSchoolInfo(appKey, "json", 1, 3, schoolName);
   }
 }
