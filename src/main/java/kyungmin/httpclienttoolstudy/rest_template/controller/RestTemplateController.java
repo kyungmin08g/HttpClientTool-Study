754e4a684f596ba07bc064d3abe710b448303aa1
Kim KyungMin <k.kyungmin2892@gmail.com>
2025-06-07 12:43:56 +0900

    [FEAT] RestTemplate을 사용하여 외부 API 보내기

--- src/main/java/kyungmin/httpclienttoolstudy/rest_template/controller/RestTemplateController.java
+++ src/main/java/kyungmin/httpclienttoolstudy/rest_template/controller/RestTemplateController.java
@@ -1,11 +1,13 @@
 package kyungmin.httpclienttoolstudy.rest_template.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import kyungmin.httpclienttoolstudy.rest_template.service.RestTemplateService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,7 +18,7 @@ public class RestTemplateController {
   private final RestTemplateService service;
 
   @GetMapping(value = "/request")
-  public ResponseEntity<Void> apiRequest() {
-    return ResponseEntity.ok().build();
+  public ResponseEntity<JsonNode> apiRequest(@RequestParam(value = "schoolName") String schoolName) {
+    return ResponseEntity.ok(service.getApiRequest(schoolName));
   }
 }
