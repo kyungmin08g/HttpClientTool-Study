a99018dd527d94312e91735ef60c7183e5f9740c
Kim KyungMin <k.kyungmin2892@gmail.com>
2025-06-07 13:29:10 +0900

    [ADD] Service 클래스 추가

--- src/main/java/kyungmin/httpclienttoolstudy/web_client/controller/WebClientController.java
+++ src/main/java/kyungmin/httpclienttoolstudy/web_client/controller/WebClientController.java
@@ -1,5 +1,6 @@
 package kyungmin.httpclienttoolstudy.web_client.controller;
 
+import kyungmin.httpclienttoolstudy.web_client.service.WebClientService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping(value = "web-client")
 public class WebClientController {
+  private final WebClientService service;
 
   @GetMapping(value = "/request")
   public ResponseEntity<Void> apiRequest(@RequestParam(value = "schoolName") String schoolName) {
---
+++ src/main/java/kyungmin/httpclienttoolstudy/web_client/service/WebClientService.java
@@ -0,0 +1,13 @@
+package kyungmin.httpclienttoolstudy.web_client.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class WebClientService {
+
+  public void getApiRequest(String schoolName) {
+
+  }
+}
