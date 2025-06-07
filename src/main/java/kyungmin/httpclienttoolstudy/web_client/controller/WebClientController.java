4c52544913dea13d5f445410139aa4cd6c1ea8af
Kim KyungMin <k.kyungmin2892@gmail.com>
2025-06-07 13:15:09 +0900

    [ADD] WebClient 요청 엔드포인트 추가

---
+++ src/main/java/kyungmin/httpclienttoolstudy/web_client/controller/WebClientController.java
@@ -0,0 +1,19 @@
+package kyungmin.httpclienttoolstudy.web_client.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "web-client")
+public class WebClientController {
+
+  @GetMapping(value = "/request")
+  public ResponseEntity<Void> apiRequest(@RequestParam(value = "schoolName") String schoolName) {
+    return ResponseEntity.ok().build();
+  }
+}
